9bd143395241a87bac566e862ef7c39ae3e748bb
Mary Ellen Klein <maryellenklein@gmail.com>
2015-12-21 09:53:09 -0600

    adding blog and contact pages

--- code/app.js
+++ code/app.js
@@ -1,22 +1,6 @@
 console.log('hello there!');
 // var $ = require('./node_modules/jquery-ui/jquery-ui.js');
 
-var ypos;
-var image;
-var image1;
-
-// function parallax () {
-//   ypos = window.pageYOffset;
-//   image = document.getElementById('parallax');
-//   image1 = document.getElementById('parallax1');
-//
-//   image.style.top = ypos * .4 + 'px'; //setting the top: property of bg image to be 40% of the normal scroll rate
-//   image1.style.top = ypos * .4 + 'px';
-// }
-//
-// window.addEventListener('scroll', parallax);
-
-
 var article = document.getElementsByName('article');
 
 $('article').hover(function() {
